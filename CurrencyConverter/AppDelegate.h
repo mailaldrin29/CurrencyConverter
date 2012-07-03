aea48c408bfab0204d2c32f6ad5a5b84b42fe584
Aldrin T <photon@Aldrins-Mac-Mini.local>
2012-07-03 12:56:39 +0530

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Aldrin Thivyanathan on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
